eed217d801e1095f6d4d76b5986c54be4bf664fd
JoseCoronel01 <30445524+JoseCoronel01@users.noreply.github.com>
2020-10-03 10:24:31 -0600

    Add files via upload

---
+++ UI/Code/BaseCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.UI;
+
+namespace UI.Code
+{
+    public class BaseCode : Page
+    {
+        public string RutaApi
+        {
+            get { return ConfigurationManager.AppSettings["RutaApi"].ToString(); }
+        }
+    }
+}
